a1386896f9cd1a73f42777b7cef7e312046ae9c5
Juan Diego CJ <166154528+JuanDiegoCJ@users.noreply.github.com>
2024-04-29 23:19:30 -0500

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return <footer>
+            <div className="footer-bottom mt-5">
+                <div className="container">
+                            <p className="text-center">© {new Date().getFullYear()} JUAN DIEGO CARRANZA JACINTO</p>
+                        </div>
+                    </div>
+    </footer>;
+}
+export default Footer;
---
+++ main.js
@@ -0,0 +1,35 @@
+const Main = () => {
+  return (
+      <main>
+<div id="carouselExample" class="carousel slide">
+<div class="carousel-inner mt-5">
+  <div class="carousel-item active">
+    <img src="https://www.elindependiente.com/wp-content/uploads/2024/01/1-2-990x660.jpg" class="d-block w-100" style={{height: "450px", width: "50%"}} alt="..."/>
+  </div>
+  <div class="carousel-item">
+    <img src="https://www.elindependiente.com/wp-content/uploads/2024/01/4-1-990x660.jpg" class="d-block w-100" style={{height: "450px", width: "50%"}} alt="..."/>
+  </div>
+  <div class="carousel-item">
+    <img src="https://www.elindependiente.com/wp-content/uploads/2024/01/9-990x660.jpg" class="d-block w-100" style={{height: "450px", width: "50%"}} alt="..."/>
+  </div>
+  <div class="carousel-item">
+    <img src="https://www.elindependiente.com/wp-content/uploads/2024/01/10-990x660.jpg" class="d-block w-100" style={{height: "450px", width: "50%"}} alt="..."/>
+  </div>
+  <div class="carousel-item">
+    <img src="https://images.samsung.com/is/image/samsung/p6pim/es/sm-a556blbceub/gallery/es-galaxy-a55-5g-sm-a556-sm-a556blbceub-540179635?$650_450_.png" class="d-block w-100" style={{height: "450px", width: "50%"}} alt="..."/>
+  </div>
+</div>
+<button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Previous</span>
+</button>
+<button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="visually-hidden">Next</span>
+</button>
+</div>
+<p>Tech-Phone es una tienda que ofrece una amplia gama de servicios para satisfacer todas tus necesidades de telefonía móvil. Desde la venta de los últimos modelos de teléfonos inteligentes hasta la reparación experta de dispositivos dañados, estamos aquí para ayudarte en cada paso del camino. Además, ofrecemos planes de servicio celular, accesorios de alta calidad y asesoramiento personalizado para garantizar que encuentres exactamente lo que necesitas para mantenerte conectado en todo momento.</p>
+      </main>
+  );
+}
+export default Main;
